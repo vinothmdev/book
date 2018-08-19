4f0cb41699595ce5a409efb3c02737d80673e13a
Vinoth Marimuthu <vinoth.m.dev@gmail.com>
2018-08-19 17:47:10 -0400

    added simple eample and correct minor grammer

---
+++ second-edition-ta/examples/hello_tamil.rs
@@ -0,0 +1,15 @@
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// இது எழிய நிரல் இரஸ்டில் எழுதுதல் எப்படி என உதாரனம் தருவதுடன்;
+// தமிழ் எழுத்துக்களை அச்சிட்டு பார்கும் முயற்ச்சியில் உருவாக்க பட்டது.
+fn main() {
+    println!("வணக்கம் தமிழ்!!!")
+}
